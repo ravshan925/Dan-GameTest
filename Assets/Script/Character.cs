a6a32d24363ddae48e85523553d83eff3d27483c
Caleb <Calebklasnic@yahoo.com>
2020-11-04 09:38:15 +0100

    Editing Game

---
+++ Assets/Script/Character.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private float moveSpeed;
+    private float dirX;
+    private bool facingRight = true;
+    private Vector3 localScale;
+
+    //Initialization
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        localScale = transform.localScale;
+        moveSpeed = 5f;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
+        if (Input.GetButtonDown("Jump") && rb.velocity.y == 0)
+            rb.AddForce(Vector2.up * 700f);
+
+        if (Mathf.Abs(dirX) > 0 && rb.velocity.y == 0)
+            anim.SetBool("isRunning", true);
+        else
+            anim.SetBool("isRunning", false);
+         
+        if(rb.velocity.y ==0)
+        {
+            anim.SetBool("isJumping", false);
+            anim.SetBool("isFalling", false);
+        }
+        if(rb.velocity.y > 0)
+        {
+            anim.SetBool("isFalling", true);
+        }    
+
+        if(rb.velocity.y < 0)
+        {
+            anim.SetBool("isJumping", false);
+            anim.SetBool("isFalling", true);
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    
+        {
+            rb.velocity = new Vector2(dirX, rb.velocity.y);
+        }
+
+
+        private void LateUpdate()
+ { 
+        if (dirX > 0)
+            facingRight = true;
+        else if (dirX < 0)
+            facingRight = false;
+
+        if (((facingRight) && (localScale.x < 0)) || ((!facingRight) && (localScale.x > 0)))
+            localScale.x *=  -1;
+
+        transform.localScale = localScale;
+    }
+}
